33648ab4af2473a9731fe0c27fa8f6982209dde9
Jose-ae <{ID}+{username}@users.noreply.github.com>
2024-08-29 19:32:56 -0600

    Init

---
+++ skiliket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skiliket
+//
+//  Created by Jose Luis on 8/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
